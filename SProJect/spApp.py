a3155782991bb6263e3e4874d2ba48036fd1a582
unknown <maritonski@gmail.com>
2018-01-15 19:32:45 +0100

    Third edit

--- SProJect/spApp.py
+++ SProJect/spApp.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
+import os, csv
 
-UPLOAD_FOLDER='C:\\Users\\Ira\\Desktop' # Do i need this ??
-ALLOWED_EXTENSIONS = set(['txt','json','csv', 'py']) # Do i need this ??
+UPLOAD_FOLDER='C:\\Users\\Ira\\Desktop\\Snaik\\10 Apps Tutorial\\SproJect'
+ALLOWED_EXTENSIONS = set(['txt','pdf','csv', 'py', 'jpg'])
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+address=[]
 
 @app.route("/", methods=['GET'])
 def index():
@@ -13,20 +15,19 @@ def index():
 
 @app.route("/templates/succcess", methods=['POST'])
 def  upload():
-    print("==== upload()")
     if request.method=='POST':
-        print("==== POST")
         data = request.files['data']
-        print(data)
-
         if '.' in data.filename and data.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
-            print("==== data.save()")
             data.save(os.path.join(app.config['UPLOAD_FOLDER'], data.filename))
-            #return render_tempalte("success.html")
-            print("==== redirect()")
-            return redirect(url_for("index"))
-        else:
-            print("==== if did not work")
+            #return render_template("success.html")
+            #return redirect(url_for("index"))
+        #else:
+            #return render_template("faliure.html")
+        with open (data) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                if row == 'address' or 'Address':
+                    print(row)
 
 
 if __name__=='__main__':
