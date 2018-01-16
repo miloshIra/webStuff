b059c701dd910f23bbb2360211d38f2a23331a49
unknown <maritonski@gmail.com>
2018-01-16 23:50:29 +0100

    failed

--- SProJect/spApp.py
+++ SProJect/spApp.py
@@ -1,34 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os, csv
-
-UPLOAD_FOLDER='C:\\Users\\Ira\\Desktop\\Snaik\\10 Apps Tutorial\\SproJect'
-ALLOWED_EXTENSIONS = set(['txt','pdf','csv', 'py', 'jpg'])
+from geopy.geocoders import Nominatim
+import pandas
+import datetime
 
 app=Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-address=[]
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route("/templates/succcess", methods=['POST'])
-def  upload():
+@app.route("/templates/success", methods=['POST'])
+def  success():
+    global filename
     if request.method=='POST':
-        data = request.files['data']
-        if '.' in data.filename and data.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
-            data.save(os.path.join(app.config['UPLOAD_FOLDER'], data.filename))
-            #return render_template("success.html")
-            #return redirect(url_for("index"))
-        #else:
-            #return render_template("faliure.html")
-        with open (data) as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            for row in reader:
-                if row == 'address' or 'Address':
-                    print(row)
+        data=request.files['data']
+        try:
+            df=pandas.read_csv(data)
+            df['coordinates']=df["Address"].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(lambda x:x.latitude if x !None else None)
+            df['Longitude'] = df['coordinates'].apply(lambda x:x.latitude if x !None else None)
+            df=df.drop("coordinates")
+            filename=datetime.datetime.now().strftime("uploads/+%Y-%m-%d-%H-%M-%S-%f"+".csv")
+            df.to_csv(filename, index=None)
+            return render_template('index.html', text=df.to_html(), btn='download.html')
+        except:
+            return render_template('index.html', text="Please enter file with address column")
 
+@app.route("/download-file/")
+def download():
+    return send_file(filename, attachment_filename='your.csv', as_attachment=True)
 
 if __name__=='__main__':
     app.debug=True
