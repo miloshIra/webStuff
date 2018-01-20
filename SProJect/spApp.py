8c11403d94565a9d04f0c2deb8ce2d94279c419c
unknown <maritonski@gmail.com>
2018-01-20 23:07:36 +0100

    Complete ...??

--- SProJect/spApp.py
+++ SProJect/spApp.py
@@ -5,26 +5,27 @@ import datetime
 
 app=Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/templates/success", methods=['POST'])
-def  success():
+@app.route("/success-table", methods=['POST'])
+def  success_table():
     global filename
     if request.method=='POST':
-        data=request.files['data']
+        file=request.files['data']
         try:
-            df=pandas.read_csv(data)
-            df['coordinates']=df["Address"].apply(gc.geocode)
-            df['Latitude'] = df['coordinates'].apply(lambda x:x.latitude if x !None else None)
-            df['Longitude'] = df['coordinates'].apply(lambda x:x.latitude if x !None else None)
-            df=df.drop("coordinates")
-            filename=datetime.datetime.now().strftime("uploads/+%Y-%m-%d-%H-%M-%S-%f"+".csv")
+            df=pandas.read_csv(file)
+            gc=Nominatim()
+            df['coordinates']=df["address"].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+            df=df.drop("coordinates",1)
+            filename=datetime.datetime.now().strftime("uploads/%Y-%m-%d-%H-%M-%S-%f"+".csv")
             df.to_csv(filename, index=None)
             return render_template('index.html', text=df.to_html(), btn='download.html')
         except:
-            return render_template('index.html', text="Please enter file with address column")
+            return render_template('index.html', text="Please enter a .csv file with address column")
 
 @app.route("/download-file/")
 def download():
