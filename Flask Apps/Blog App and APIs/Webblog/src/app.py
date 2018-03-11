b06be98cc466df2ae99efe18cdddd2579050c209
Milosh Aritonski <maritonski@gmail.com>
2018-03-11 19:19:58 +0100

    Posts but doesnt work for now will fix it later

--- Flask Apps/Blog App and APIs/Webblog/src/app.py
+++ Flask Apps/Blog App and APIs/Webblog/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from models.user import User
+from models.blog import Blog
 from common.database import Database
 
 
@@ -56,6 +57,12 @@ def user_blogs(user_id=None):
     blogs=user.get_blogs()
     return render_template("user_blogs.html", blogs=blogs, email=user.email)
 
+@app.route('/posts/string:blog_id')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts
+
+    return render_template('posts.html', posts=posts, blog_title=blog.title)
 
 if  __name__=='__main__':
     app.run(port=4995)
