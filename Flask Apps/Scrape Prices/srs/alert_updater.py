a6194a3b8d4e949d306dc940c038a2e63d4898a7
miloshIra <maritonski@gmail.com>
2018-05-04 22:45:20 +0200

    asda

--- Flask Apps/Scrape Prices/srs/alert_updater.py
+++ Flask Apps/Scrape Prices/srs/alert_updater.py
@@ -6,5 +6,6 @@ Database.initialize()
 alerts_needing_update = Alert.find_needing_update()
 
 for alert in alerts_needing_update:
+
     alert.load_item_price()
     arelrt.send_email_if_price_reached()
--- Flask Apps/Scrape Prices/srs/tdd.py
+++ Flask Apps/Scrape Prices/srs/tdd.py
@@ -12,3 +12,11 @@ pattern = re.compile("(\d+,\d+)") # 155.00
 match = pattern.search(string_price)
 
 print(match.group())
+
+
+
+# use fullstack
+# db.users.insert({"_id": "123", "email": "Iradan123@gmail.com", "password": "$pbkdf2-sha256$7665$WKs1ZkwJ4ZxT6t07R0iplQ$ZKfMMAMzKxH64g.3XwaFONAlVwoZf76dWdqW6uSlQtE"})
+# db.items.insert({"_id": "d5527d22c0a74a8199fbbc0aab44046", "url": "https://www.bookdepository.com/Learning-Python-Mark-Lutz/9781449355739?ref=grid-view&qid=1525427233471&sr=1-1", "name": "Learning Python" })
+# db.alerts.insert({"_id": "896045e647084cacb37a702f418be70", "price_limit": 100, "last_checked": ISODate("2016-02-09T10:35:31.542Z"), "item_id": "d5527d22c0a74a8199fbbc0aab440463", "user_email": "iradan123@gmail.com"})
+# db.stores.insert({ "_id" : "253217ddddf74923852cf3f84f9b48b8", "name" : "Book Depository", "url_prefix" : "https://www.bookdepository.com", "tag_name" : "span", "query" : { "class" : "sale-price" } })
