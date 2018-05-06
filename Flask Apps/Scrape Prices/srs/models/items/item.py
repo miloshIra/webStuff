d947401e79b82eda597f5989e9092ac6202dd89f
miloshIra <maritonski@gmail.com>
2018-05-06 22:26:01 +0200

    re-did the alerts... seems to be ok now will check tomorrow

--- Flask Apps/Scrape Prices/srs/models/items/item.py
+++ Flask Apps/Scrape Prices/srs/models/items/item.py
@@ -11,9 +11,9 @@ class Item(object):
         self.name = name
         self.url = url
         store = Store.find_by_url(url)
-        tag_name = store.tag_name
-        query = store.query
-        self.price = self.load_price(tag_name, query)
+        self.tag_name = store.tag_name
+        self.query = store.query
+        self.price = None
         self._id = uuid.uuid4().hex if _id is None else _id
 
 
@@ -25,13 +25,14 @@ class Item(object):
         request = requests.get(self.url)
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
-        element = soup.find(tag_name, query)
+        element = soup.find(self.tag_name, self.query)
         string_price = element.text.strip()
 
         pattern = re.compile("(\d+.\d+)") # 155.00
         match = pattern.search(string_price)
+        self.price = match.group()
 
-        return match.group()
+        return self.price
 
     def save_to_mongo(self):
         Database.insert(ItemConstants.COLLECTION, self.json())
@@ -44,3 +45,7 @@ class Item(object):
             "url": self.url,
             "_id": self._id
         }
+
+    @classmethod
+    def get_by_id(cls, item_id):
+        return cls(**Database.find_one(ItemConstants.COLLECTION, {"_id": item_id}))
