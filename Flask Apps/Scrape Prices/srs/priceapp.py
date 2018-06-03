0727e0f6130a039453653281efb077d41cdff2d2
miloshIra <maritonski@gmail.com>
2018-06-03 18:38:51 +0200

    started FrontEND kwa kwa kwaaaaaa

--- Flask Apps/Scrape Prices/srs/priceapp.py
+++ Flask Apps/Scrape Prices/srs/priceapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from common.database import Database
 
 
@@ -9,6 +9,10 @@ app.secret_key = "123"
 @app.before_first_request
 def init_db():
     Database.initialize()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 from models.users.views import user_blueprint
 app.register_blueprint(user_blueprint, url_prefix="/users")
