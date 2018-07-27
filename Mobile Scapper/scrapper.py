e392867a696b50b40c456b045fdb98713c70f980
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2018-07-27 07:27:40 +0200

    Add files via upload
    Find doesn't work .. but it should

---
+++ Mobile Scapper/scrapper.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.support.ui import Select
+
+def load_countries():
+    global z
+    request = requests.get("https://mobilerecharge.com/buy/mobile_recharge?country=Afghanistan&operator=Etisalat")
+    content = request.content
+    # print(content)
+    soup = BeautifulSoup(content,"html.parser")
+    state = soup.find("select", {"id":"international_country"})
+    count = (state.text.strip()).split()
+    z = count[2:]
+load_countries()
+
+
+driver = webdriver.Chrome()
+driver.get("https://mobilerecharge.com/buy/mobile_recharge?country=Afghanistan&operator=Etisalat")
+
+def list_contries():
+
+    select = Select(driver.find_element_by_id('international_country'))
+    select.select_by_visible_text('France')
+    request = requests.get("https://mobilerecharge.com/buy/mobile_recharge?country=Afghanistan&operator=Etisalat")
+    content = request.content
+    soup = BeautifulSoup(content, "html.parser")
+    prov=soup.find("div", {"class": "operator deselected"})['data-operator_name']
+    operator = prov.text.strip()
+    print(operator)
+
+list_contries()
