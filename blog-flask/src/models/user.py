2e4e295dee7d709a436a479da1a626d9d8796dfe
miloshIra <maritonski@gmail.com>
2021-11-08 20:58:03 +0100

    Just trying stuff Flask throwback, maybe I am skilled enough to run the blog again.

--- blog-flask/src/models/user.py
+++ blog-flask/src/models/user.py
@@ -1,10 +1,12 @@
-from src.common.database import Database
+import datetime
 import uuid
 from flask import session
+from src.common.database import Database
+from src.models.blog import Blog
 
 
-class User:
-    def __int__(self, email, password, _id=None):
+class User(object):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
         self._id = uuid.uuid4().hex if _id is None else _id
@@ -23,8 +25,10 @@ class User:
 
     @staticmethod
     def login_valid(email, password):
+        # Check whether a user's email matches the password they sent us
         user = User.get_by_email(email)
         if user is not None:
+            # Check the password
             return user.password == password
         return False
 
@@ -32,15 +36,18 @@ class User:
     def register(cls, email, password):
         user = cls.get_by_email(email)
         if user is None:
+            # User doesn't exist, so we can create it
             new_user = cls(email, password)
             new_user.save_to_mongo()
             session['email'] = email
             return True
         else:
+            # User exists :(
             return False
 
     @staticmethod
     def login(user_email):
+        # login_valid has already been called
         session['email'] = user_email
 
     @staticmethod
@@ -48,10 +55,22 @@ class User:
         session['email'] = None
 
     def get_blogs(self):
+        return Blog.find_by_author_id(self._id)
 
+    def new_blog(self, title, description):
+        blog = Blog(author=self.email,
+                    title=title,
+                    description=description,
+                    author_id=self._id)
 
-    def save_to_mongo(self):
-        Database.insert("users", self.json())
+        blog.save_to_mongo()
+
+    @staticmethod
+    def new_post(blog_id, title, content, date=datetime.datetime.utcnow()):
+        blog = Blog.get_from_mongo(blog_id)
+        blog.new_post(title=title,
+                      content=content,
+                      date=date)
 
     def json(self):
         return {
@@ -59,3 +78,6 @@ class User:
             "_id": self._id,
             "password": self.password
         }
+
+    def save_to_mongo(self):
+        Database.insert("users", self.json())
