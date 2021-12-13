ac20a001220d9c0b7b7f692c2d18c28c65b9399f
miloshIra <maritonski@gmail.com>
2021-12-13 18:15:18 +0100

    Split|Crop project going web.

---
+++ splitcrop-web/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "Ira"
+
+
+@app.route('/')
+def start_template():
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home_template():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(port=1000, debug=True)
+
