9360a91f590e4ee4a56b588afee4e76f089142c3
miloshIra <maritonski@gmail.com>
2022-04-11 12:28:14 +0200

    FSP Chapter 9 - Pagination Just doing stuff not to get rusty, fixed tests, worked on following and followers, it's starts getting hairy for me.

--- fsp-tut/app/routes.py
+++ fsp-tut/app/routes.py
@@ -24,10 +24,16 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
-        return render_template('index.html', title='Home Page')
+        return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
-    return render_template("index.html", title='Home page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Home page', form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -73,10 +79,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = Post.query.filter_by(user_id=current_user.id).all()
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
 
@@ -140,5 +143,11 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
